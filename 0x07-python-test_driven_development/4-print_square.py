2732ee708905da86609fa800c7ee8873307f8426
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-08 11:56:52 -0700

    tests

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""print_square
+
+"""
+
+
 def print_square(size):
+    """print_square
+    arg:
+        size: size of square
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
