f7cd636e14d6b9db70b3955db003f98360ad65a9
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-09 13:45:14 -0700

    inheritance-in- python

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """MyList class"""
+
+
 class MyList(list):
     """ MyList class"""
 
     def print_sorted(self):
         """print_sorted function"""
-        
+
         new_list = sorted(self)
         print(new_list)
