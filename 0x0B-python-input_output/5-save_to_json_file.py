66f12d4ef3039a6862f43a57a210350520e53dd3
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-11 12:01:42 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ def save_to_json_file(my_obj, filename):
         using a JSON representation
     """
     with open(filename, 'w', encoding="utf=8") as f:
-        f = json.dumps(my_obj)
+        f = json.dump(my_obj)
         return f
