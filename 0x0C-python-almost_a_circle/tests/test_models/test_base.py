6472da58d7cb7eb7feaa93dc3828b55d62f1dce1
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-16 14:44:33 -0700

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+class BaseTestCases(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
+    def test_nb_obj(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
