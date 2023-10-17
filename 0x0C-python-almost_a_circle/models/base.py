fa7ec7cdf574eabed8c736373938054235117730
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-17 04:24:05 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,9 +2,12 @@
 """models/base.py"""
 import json
 import os
+
+
 class Base:
     """Base class of the other shapes"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """init method"""
         if id is not None:
@@ -12,29 +15,33 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns JSON string representation"""
-        if list_dictionaries == [] or list_dictionaries is None:
-            return list_dictionaries == []
+        if list_dictionaries is None:
+            return list_dictionaries == "[]"
         return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes JSON string repr to file"""
-        if list_objs is None:
-            return list_objs == []
-        else:
-            json_string = \
-                Base.to_json_string([obj.to_dictionary() for obj in list_objs])
-            filename = Base.__name__ + ".json"
-            with open(filename, 'w', encoding='utf-8') as file:
-                file.write(json_string)
+        filname = f"{cls.__name__}.json"
+        new_list = []
+        if list_objs:
+            for i in list_objs:
+                new_list.append(cls.to_dictionary(i))
+        json_str = cls.to_json_string(new_list)
+
+        with open(filname, "w", encoding='utf-8') as file:
+            file.write(json_str)
+
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation"""
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
     @classmethod
     def create(cls, **dictionary):
         """Returns instance with all attributes already set"""
@@ -44,6 +51,7 @@ class Base:
             dummy = cls(3)
         dummy.update(**dictionary)
         return dummy
+
     @classmethod
     def load_from_file(cls):
         filename = "{}.json".format(cls.__name__)
